12c00e4c1b448e6d3b3b7f9cee9cac32591102fe
ponsfrilus <ponsfrilus@gmail.com>
2015-04-15 10:35:41 +0200

    CLean up

--- ubuntupostinstall.sh
+++ ubuntupostinstall.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 ##
-## @title   Post Install *buntu
-## @author  ponsfrilus
-## @email   ponsfrilus@gmail.com
-## @date    2013-05-29
-## @version 0.0.1
+## @title     Post Install *buntu
+## @author    ponsfrilus
+## @email     ponsfrilus@gmail.com
+## @create_dt 2013-05-29
+## @Update_dt 2015-04-15
+## @version   0.0.2
 ##
 ## Usage:
-##  chmod +x post-install.sh
-##  sudo ./post-install.sh
+##  chmod +x ubuntupostinstall.sh
+##  sudo ./ubuntupostinstall.sh
 
 
 ## http://tldp.org/LDP/abs/html/
 
 if [ ! -w /etc/passwd ]; then
-    echo "Super-user privileges are required.  Please run this with 'sudo ./post-install.sh'." >&2
+    echo "Super-user privileges are required. Please run this with 'sudo ./ubuntupostinstall.sh'." >&2
     exit 1
 fi
 
 show_title() {
     clear;
-    echo "  ++ponsfrilus++";
-    echo "   ___          _      _____           _        _ _ ";
+    echo "=====================================================";
+    echo "";
+    echo "   __ponsfrilus _      _____           _        _ _ ";
     echo "  / _ \___  ___| |_    \_   \_ __  ___| |_ __ _| | |";
     echo " / /_)/ _ \/ __| __|    / /\/ '_ \/ __| __/ _\` | | |";
     echo "/ ___/ (_) \__ \ |_  /\/ /_ | | | \__ \ || (_| | | |";
     echo "\/    \___/|___/\__| \____/ |_| |_|___/\__\__,_|_|_|";
     echo "        make the installation of *buntu stuff easier";
     echo "";
+    echo "=====================================================";
 }
 
 # Menu 1 : Dev Tools
-menu1() { 
+menu1() {
     sudo apt-get install -y gedit vim git markdown;
     export EDITOR=/usr/bin/vim
     echo "installation of Sublime Text";
@@ -43,7 +46,7 @@ menu1() {
     sudo apt-get install apache2 php5 mysql-server php5-mysql php5-curl php5-ldap
     echo "sudo a2enmod rewrite ldap"
     sudo service apache2 restart
-    #sudo a2enmod 
+    #sudo a2enmod
 
     ## Not really dev tools but to mount smb share in fstab
     ## Read http://askubuntu.com/questions/157128/proper-fstab-entry-to-mount-a-samba-share-on-boot
@@ -58,12 +61,12 @@ menu1() {
         password=YourPassword
         domain=YourDomain
         ";
-        echo "Then chmod 0600 ~/.smbcredentials"; 
+        echo "Then chmod 0600 ~/.smbcredentials";
     echo "//server/share /pathto/mountpoint cifs credentials=/home/username/.smbcredentials,uid=shareuser,gid=sharegroup 0 0"
-    sudo apt-get install rdesktop
+    sudo apt-get install -y rdesktop
     # Android
-    sudo apt-get install openjdk-7-jdk
-    sudo apt-get install android-tools-fastboot && sudo apt-get install android-tools-adb
+    sudo apt-get install -y openjdk-7-jdk
+    sudo apt-get install -y android-tools-fastboot && sudo apt-get install android-tools-adb
 # sudo apt-get install xrdp
 
 ## Git migration (https://github.com/nirvdrum/svn2git)
@@ -73,25 +76,23 @@ menu1() {
 # Menu 2 : Geek Stuff
 menu2() {
         echo "installing ZSH";
-        sudo apt-get install zsh
+        sudo apt-get install -y zsh
         echo "change the default shell (answer /bin/zsh)";
-    sh -s /bin/zsh 
+    sh -s /bin/zsh
         echo "Read https://github.com/robbyrussell/oh-my-zsh#readme";
         echo "installing oh my zsh";
         wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
     echo "In EPFL, you can change you default shell in "
     echo "https://dinfo.epfl.ch/cgi-bin/accountprefs"
-    sudo apt-get install openssh-server
+    sudo apt-get install -y openssh-server
     # http://dinozaur1982.deviantart.com/art/conky2-316541287
-    sudo apt-get install conky
-
-
+    sudo apt-get install -y conky
 }
 
 # Menu 3 : Internet / Desktop
 menu3(){
-    sudo apt-get install libreoffice
-    sudo apt-get install chromium-browser   
+    sudo apt-get install -y libreoffice
+    sudo apt-get install -y chromium-browser
     #sudo apt-get install dropbox
     #Install KeepNote.org
     #Intall markdown GUI / Gummy
@@ -100,7 +101,7 @@ menu3(){
 # Menu 4: Multimedia
 menu4(){
     echo "multimedia stuff";
-    sudo apt-get install vlc ffmpeg mplayer dvdrip
+    sudo apt-get -y install vlc ffmpeg mplayer dvdrip
     # Spotify
     sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list'
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
@@ -112,13 +113,13 @@ menu4(){
 # Menu 5: Drawing
 menu5(){
     echo "Drawing stuff";
-    sudo apt-get install inkscape gimp gthumb pinta gpicks
+    sudo apt-get install -y inkscape gimp gthumb pinta gpicks
 }
 
 # Menu 6: Confort
 menu6(){
     echo "Confort stuff";
-    #sudo apt-get install inkscape gimp gthumbs pinta 
+    #sudo apt-get install inkscape gimp gthumbs pinta
 ## CONFORT
 ## workrave
 
@@ -143,14 +144,10 @@ menu7(){
 
 # Menu 8: Optimization
 menu8(){
-    echo "Drawing stuff";
-    sudo apt-get install inkscape gimp gthumb pinta 
+    echo "Optimization";
+    sudo apt-get autoremove
+    sudo apt-get autoclean
 }
-## OPTIMIZATION
-## Cleaning UP
-#sudo apt-get autoremove
-## preload
-## sudo apt-get autoclean
 
 
 show_menu() {
@@ -160,7 +157,7 @@ show_menu() {
     options=("Dev-Tools"    "Geek Stuff"    "Desktop / Internet"    "Multimedia"    "Drawing"   "Confort"   "Multi-Screen"  "Optimization")
     echo "Please select your objectives : "
     PS3="Pick an option (press enter to see choices): "
-    select opt in "${options[@]}" "All" "Quit (or q)"; do 
+    select opt in "${options[@]}" "All" "Quit (or q)"; do
         case "$REPLY" in
             1 )     echo "$opt: will install gedit, vim, git, markdown, sublime"
                 while true
@@ -197,7 +194,7 @@ show_menu() {
                                                 esac
                                         done
                                 ;;
-            
+
             4 ) echo "$opt: will install vlc, mplayer, ffmpeg"
                                 while true
                                         do
@@ -257,9 +254,9 @@ show_menu() {
                                                 esac
                                         done
                                 ;;
-            
+
             $(( ${#options[@]}+1 )) ) echo "You are a crazy monkey !"
-                        menu1 
+                        menu1
                         menu2
                         menu3
                         menu4
@@ -283,46 +280,3 @@ show_title
 show_menu
 
 exit 0
-# Update depots
-#sudo apt-get update
-
-## http://www.webupd8.org/2012/11/how-to-use-multiple-monitors-in-xubuntu.html
-## Make the Multiscreen available + Thunar Tabs
-#sudo add-apt-repository ppa:xubuntu-dev/xfce-4.12
-#sudo apt-get update
-#sudo apt-get upgrade
-#echo "Now run the following command to configure your displays:"
-#echo "xfce4-display-settings -m"
-#echo "And run the fillowing command to restart Thunar and have tabs support:"
-#echo "thunar -q"
-
-## OPTIMIZATION
-## Cleaning UP
-#sudo apt-get autoremove
-## preload
-## sudi apt-get autoclean
-
-
-## DEV TOOLS
-#sudo apt-get install vim git markdown
-## export EDITOR="vim"
-
-## GEEK Tools
-#sudo apt-get install guake 
-## oh my zhs, zhs
-
-## INTERNET TOOLS
-## chrome, opera
-
-## MULTIMEDIA TOOLS
-## VLC sudo apt-get install dvdrip
-## sudo /usr/share/doc/libdvdread4/install-css.sh
-
-
-## DRAWING TOOLS
-#pinta gthumb
-
-
-
-
-
